17a8ad4a4e395ecab39f4253b68cdd1eb5062896
Jonathan <jonathanadalin@gmail.com>
2019-02-18 20:15:02 -0600

    Added algorithm with runtime O(cols*rows)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-// Helper Function
 void PrintMaze(bool** maze, int cols, int rows) {
     for (int j = 0; j < rows; j++) {
         std::cout << std::endl;
@@ -13,11 +12,47 @@ void PrintMaze(bool** maze, int cols, int rows) {
         }
     }
     // Make the console look nicer.
-    std::cout << std::endl;
-    std::cout << std::endl;
+    std::cout << std::endl << std::endl;
 }
 
-// Algorithm Driver
+// Algorithm Helper Functions
+
+bool LeftOK(bool** maze, int x, int y) {
+    if (x - 1 >= 0)  // Don't want to access an element out of bounds.
+        if (maze[x - 1][y] == true)
+            return true;
+    return false;
+}
+
+bool AboveOK(bool** maze, int x, int y) {
+    if (y - 1 >= 0)  // Don't want to access an element out of bounds.
+        if (maze[x][y - 1] == true)
+            return true;
+    return false;   
+}
+
+// Algorithm: Count Paths, Runtime: O(cols*rows)
+// Returns the number of ways to get from the top-left corner to the
+// bottom-right corner of <maze> assuming you can only go right or down.
+int CountPaths(bool** maze, int cols, int rows) {
+	int dp[cols][rows];
+	for (int i = 0; i < cols; i++) {
+		for (int j = 0; j < rows; j++) {
+			if (i == 0 && j == 0)
+				dp[i][j] = 1;
+			else if (AboveOK(maze, i, j) && LeftOK(maze, i, j))
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+			else if (AboveOK(maze, i, j))
+				dp[i][j] = dp[i][j - 1];
+			else if (LeftOK(maze, i, j))
+				dp[i][j] = dp[i - 1][j];
+			else  // This tile is unaccessible.
+				dp[i][j] = 0;
+		}
+	}
+	return dp[cols-1][rows-1];
+}
+
 int main() {
 
     int cols = 4;
@@ -27,11 +62,16 @@ int main() {
     for (int i = 0; i < cols; i++)
         maze[i] = new bool[rows];
 
+    // Every tile should be accessible at first.
     for (int i = 0; i < cols; i++)
         for (int j = 0; j < rows; j++)
             maze[i][j] = true;
+    
+    // Add maze blocks here if desired.
     maze[2][1] = false;
 
     PrintMaze(maze, cols, rows);
+    std::cout << "Paths: " << CountPaths(maze, cols, rows)
+              << std::endl << std::endl;
 }
 
